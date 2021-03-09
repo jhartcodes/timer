0694c26d743cd65e50a6b939ce153feb563afd78
Joel <joel.d.hart1@gmail.com>
2021-03-09 23:49:03 +0000

    'new timer code'

---
+++ timer1.js
@@ -0,0 +1,11 @@
+
+const time = process.argv.slice(2);
+
+for (const item in time) {
+  if (time[item] > 0) {
+    setTimeout(() => {
+      process.stdout.write('beep\n')
+    }, time[item] * 1000);
+  };
+};
+
